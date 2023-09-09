1964cdbc8481d9884a6253b75d50a2c5a395810b
Tony Varghese <tonyvar666@gmail.com>
2023-09-09 19:40:36 +0530

    all the buttons added ready to use

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def btn_clear():
     input_text.set("")
 
 def btn_equal():
+    global expression
     result=str(eval(expression))
     input_text.set(result)
     expression=""
@@ -34,11 +35,24 @@ btns_frame.pack()
 clear = Button(btns_frame,text="clear",fg="black",width = 32,height=3,bd=0,bg="#eee",cursor="hand2", command=lambda: btn_clear()).grid(row=0,column=0,columnspan=3,padx=1,pady=1)
 divide = Button(btns_frame,text="/",fg="black",width=10,height=3,bd=0,bg="#eee",cursor="hand2",command= lambda:btn_click("/")).grid(row=0,column=3,padx=1,pady=1)
 
+seven = Button(btns_frame,text="7",fg="black",width=10,height=3,bd=0,bg="#fff",cursor="hand2",command=lambda :btn_click("7")).grid(row=1,column=0,padx=1,pady=1)
+eight= Button(btns_frame,text="8",fg="black",width=10,height=3,bd=0,bg="#fff",cursor="hand2",command=lambda :btn_click("8")).grid(row=1,column=1,padx=1,pady=1)
+nine = Button(btns_frame,text="9",fg="black",width=10,height=3,bd=0,bg="#fff",cursor="hand2",command=lambda :btn_click("9")).grid(row=1,column=2,padx=1,pady=1)
+multiply = Button(btns_frame,text="x",fg="black",width=10,height=3,bd=0,bg="#fff",cursor="hand2",command=lambda :btn_click("*")).grid(row=1,column=3,padx=1,pady=1)
 
+four = Button(btns_frame,text="4",fg="black",width=10,height=3,bd=0,bg="#fff",cursor="hand2",command=lambda :btn_click("4")).grid(row=2,column=0,padx=1,pady=1)
+five = Button(btns_frame,text="5",fg="black",width=10,height=3,bd=0,bg="#fff",cursor="hand2",command=lambda :btn_click("5")).grid(row=2,column=1,padx=1,pady=1)
+six = Button(btns_frame,text="6",fg="black",width=10,height=3,bd=0,bg="#fff",cursor="hand2",command=lambda :btn_click("6")).grid(row=2,column=2,padx=1,pady=1)
+minus = Button(btns_frame,text="-",fg="black",width=10,height=3,bd=0,bg="#fff",cursor="hand2",command=lambda :btn_click("-")).grid(row=2,column=3,padx=1,pady=1)
 
+one = Button(btns_frame,text="1",fg="black",width=10,height=3,bd=0,bg="#fff",cursor="hand2",command=lambda :btn_click("1")).grid(row=3,column=0,padx=1,pady=1)
+two = Button(btns_frame,text="2",fg="black",width=10,height=3,bd=0,bg="#fff",cursor="hand2",command=lambda :btn_click("2")).grid(row=3,column=1,padx=1,pady=1)
+three = Button(btns_frame,text="3",fg="black",width=10,height=3,bd=0,bg="#fff",cursor="hand2",command=lambda :btn_click("3")).grid(row=3,column=2,padx=1,pady=1)
+plus = Button(btns_frame,text="+",fg="black",width=10,height=3,bd=0,bg="#fff",cursor="hand2",command=lambda :btn_click("+")).grid(row=3,column=3,padx=1,pady=1)
 
-
-
+zero = Button(btns_frame,text="0",fg="black",width=10,height=3,bd=0,bg="#fff",cursor="hand2",command=lambda :btn_click("0")).grid(row=4,column=0,columnspan=2,padx=1,pady=1)
+point = Button(btns_frame,text=".",fg="black",width=10,height=3,bd=0,bg="#fff",cursor="hand2",command=lambda :btn_click(".")).grid(row=4,column=2,padx=1,pady=1)
+equals = Button(btns_frame,text="=",fg="black",width=10,height=3,bd=0,bg="#fff",cursor="hand2",command=lambda :btn_equal()).grid(row=4,column=3,padx=1,pady=1)
 
 
 root.mainloop()
